033c480b101c99377aa6c1f79a4f3b471379106d
ernesto-epppn <ilio.toulouse@gmail.com>
2026-09-01 12:25:19 +0200

    Load final Action mode visual override

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ import "./ernesto-v14-5-2.css";
 import "./ernesto-v14-5-3.css";
 import "./ernesto-ui-polish.css";
 import "./ernesto-context-meter.css";
+import "./ernesto-mode-override.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
